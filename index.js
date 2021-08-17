840e4805e094bff767fbe588e4b9048505e2311d
lekshmi sv <lekshmisv1998@gmail.com>
2021-08-17 21:39:45 +0530

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express=require('express');
-const app=express();
-app.listen(3000,(error)=>{
+const express = require('express');
+const mainrouter = require('./router/router');
+const app = express();
+
+app.use('/',mainrouter);  
+app.listen(3000,(error,result)=>{
     if(error){
         return console.log(error);
-    }
+    
+} 
+       
     console.log('App running on localhost 3000');
-});
+    });
+    
 
-app.get('/',(req,res)=>{
-    res.send("hello world");
-});
---
+++ router/router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = new express.Router();
+
+router.get('/',(req,res)=>{
+    res.send("hlo world");
+});
+module.exports=router;
